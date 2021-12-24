d44c97f15eb295a27cc9b5655d638eeddb2ad567
Avani Patel <avanipatel.sculptsoft@gmail.com>
2021-12-24 14:12:08 +0530

    Structure error

--- Demoapplication/Startup.cs
+++ Demoapplication/Startup.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Demoapplication
 {
@@ -42,6 +43,8 @@ namespace Demoapplication
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demoapplication v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
